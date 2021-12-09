7e4aff421733b1ab307eca3f08ed64e8953607f7
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2021-12-09 19:56:19 +0530

    Set alacritty as default

--- rc.lua
+++ rc.lua
@@ -55,7 +55,7 @@ end
 beautiful.init(gears.filesystem.get_configuration_dir() .."theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "konsole"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
