d86085c20193204c4c0bc464572a0f649d4faf55
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-07-16 20:56:06 +0530

    Added the license to the rc.lua

--- rc.lua
+++ rc.lua
@@ -1,3 +1,20 @@
+--[[
+    awesomewm
+    Copyright (C) 2023  Srivathsan Sudarsanan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
